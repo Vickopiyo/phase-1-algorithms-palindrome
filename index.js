d6253e304cd5b08a3f22cd26320e641506c3e937
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-16 11:34:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const re=/[\W_]/g;
+
+
+  const lowWord=word.toLowerCase().replace(re,"");
+  const reverseWord=lowWord.split("").reverse().join("");
+  if(reverseWord===lowWord){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 
