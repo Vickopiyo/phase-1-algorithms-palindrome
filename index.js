d9905e0451b252c617183f8c7a73694a5fb35b02
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-16 11:42:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // converts every non alpahnumeric characters
   const re=/[\W_]/g;
 
-
+// LOWWord converts any string passsed to lowercase and any non non alpahnumeric function is replaced with no space
   const lowWord=word.toLowerCase().replace(re,"");
+  // checks the reverse of word is after spliting each character and later join 
   const reverseWord=lowWord.split("").reverse().join("");
   if(reverseWord===lowWord){
     return true;
